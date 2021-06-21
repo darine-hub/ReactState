a2235d7b4a23dea41ab97dd48f63c57e46b936ed
darine <benmahmouddarine@gmail.com>
2021-06-21 13:32:24 +0200

    add application react state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,65 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Profile from "./component/Profile"
-import Timer from './component/Timer';
+import Profile from "./component/Profile";
+import myphoto from "./photo.jpg"
+//import Timer from './component/Timer';
 
-function App() {
-
-  const container= {
-    "marginTop":"120px",
-    "background": "#ADD8E6",
-   
-    "padding": "30px 80px",
-    "maxWidth": "500px",
-    "margin": "0 auto",
-    "boxShadow": "0 15px 16px .17px rgba(0,0,0,0.05)",
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false, 
+      person :{
+     imagesrc: myphoto,
+      imagealt: "photo",
+      fullName: "Darine Ben Mahmoud",
+      profession: "Devloppeur web full stack",
+      bio: "je suis un developpeur web front ent et back end ,je maitrise la bibliotheque react ",
+     
     
     
-  
+    }
+    };
   }
+ 
+  toggleVisibility= ()=>{
+    if(this.state.visibility===false){
+      this.setState({visibility:true})
+    }
+    else{
+      this.setState({visibility:false})
+    }
+   
+    }
+
+    render() {
+    
+      const container= {
+        marginTop:"120px",
+        background: "#ADD8E6",
+       
+        padding: "30px 80px",
+        maxWidth: "500px",
+        margin: "0 auto",
+        boxShadow: "0 15px 16px .17px rgba(0,0,0,0.05)",
+        
+        
+      
+      }
+
   return (
-    <div style={container}>
-    <Profile/>
-    <Timer/>
+    <div style={container }>
+
+{this.state.visibility && <Profile person={this.state.person} />}
+    <button type="button" onClick={this.toggleVisibility}>
+           Profile
+        </button>
+    
     </div>
     
   );
-}
+}   }
 
 export default App;
--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,65 +1,55 @@
 import React from "react";
-import myphoto from "./photo.jpg"
+
 
 class Profile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-     imagesrc: myphoto,
-      imagealt: "photo",
-      fullName: "Darine Ben Mahmoud",
-      profession: "Devloppeur web full stack",
-      bio: "je suis un developpeur web front ent et back end ,je maitrise la bibliotheque react ",
-      show: true,
-      
-    };
-  }
-
-  showProfile = () => {
-    if (this.state.show === true) {
-      this.setState({
-        imagesrc: "",
-        imagealt:"",
-        fullName: "",
-        profession: "",
-        bio: "",
-        show: false,
-      });
-    } else {
-      this.setState({
-        imagesrc: myphoto,
-        fullName: "Darine ben mahmoud",
-        profession: "Devloppeur web full stack",
-        bio: "je suis un developpeur web front ent et back end ,je maitrise la bibliotheque react",
-        show: true,
-      });
+  
+  constructor (props){
+    super (props);
+    this.state={
+      timer:0
     }
-  };
+  }
+ 
 
   render() {
+    
       const styleh = {
 color:"#191970"
 
 
       }
+    
    
     return (
       <div>
-        <img src={this.state.imagesrc} alt={this.state.imagealt} />
+        <img src={this.props.person.imagesrc} alt={this.props.person.imagealt} />
         <br/>
         <br/>
         <h4 style={styleh}> NAME:</h4>
-        <h5>{this.state.fullName}</h5>
+        <h5>{this.props.person.fullName}</h5>
         <h4 style={styleh}> Profession:</h4>
-        <h5>{this.state.profession}</h5> <h4 style={styleh}> BIO:</h4>
-        <h5>{this.state.bio}</h5>
+        <h5>{this.props.person.profession}</h5> <h4 style={styleh}> BIO:</h4>
+        <h5>{this.props.person.bio}</h5>
         <br></br>
-        <button type="button" onClick={this.showProfile}>
-          Change Profile
-        </button>
+      
+       
+        <h5>
+    current count:{this.state.timer}
+</h5>
        
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(
+      () => this.setState({ timer:this.state.timer+1 }),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.interval);
   }
  
 }
